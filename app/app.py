3c4a4c76ffba0bb9302bc4f7c0185f7930e25a7e
zAndreyG <andreygo20@gmail.com>
2020-04-22 20:03:21 -0300

    Change to Flask-PyMongo

--- app/app.py
+++ app/app.py
@@ -1,51 +1,86 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 from flask_cors import CORS
-import pymongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 import mongoapp as mongoapp
 
 app = Flask(__name__)
 CORS(app)
+app.secret_key = "secretkey"
+app.config['MONGO_URI'] = "mongodb://andrey:mongopass@cluster0-shard-00-00-ccvwf.mongodb.net:27017,cluster0-shard-00-01-ccvwf.mongodb.net:27017,cluster0-shard-00-02-ccvwf.mongodb.net:27017/testpy?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-@app.route('/vers/create', methods=['POST'])
+mongo = PyMongo(app)
+
+@app.route('/create', methods=['POST'])
 def insert_vers():
-    client = mongoapp.connection()
-    db = client["testpy"]
-    coll = db["test_collection"]
+    if isinstance(request.json, list) == False:
+        _json = request.json
+        _author = _json['autor']
+        _text = _json['texto']
 
-    if request.json:
+        if _author and _text:
+            try:
+                _id = mongo.db.test_collection.insert({
+                    'autor': _author,
+                    'texto': _text
+                })
+
+                resp = jsonify('Verse added successfully')
+                resp.status_code = 200
+                return resp
+            except: return jsonify('Bad Request')
+
+    else: return jsonify('No data')
+
+    """ elif isinstance(request.json, list):
+        client = mongoapp.connection()
+        coll = client["test_collection"]
+    
         try:
-            if len(request.json) == 1:
-                db_response = mongoapp.create_one(coll, request.json[0])
-                return str(db_response.inserted_id)
+            _ids = mongoapp.create_many(coll, request.json)
 
-            elif len(request.json) > 1:
-                db_response = mongoapp.create_many(coll, request.json)
-                return str(db_response.inserted_ids)
+            resp = jsonify('Verses added successfully')
+            resp.status_code = 200
+            return resp #str(db_response.inserted_ids)
 
-        except: return 'Bad Request: The data must be a JSON Array'
-    else: return "No data"
+        except: return jsonify('Bad Request: Verify if the data is a JSON Array') """
 
-@app.route('/vers/list', methods=['GET'])
-def select_vers():
-    client = mongoapp.connection()
-    db = client["testpy"]
-    coll = db["test_collection"]
+@app.route('/list', methods=['GET'])
+def list_vers():
+    verses = mongo.db.test_collection.find()
 
-    db_response = mongoapp.select_all(coll)
-    return str(db_response)
+    resp = dumps(verses)
+    return resp
 
-@app.route('/vers/update', methods=['POST'])
-def update_vers():
-    client = mongoapp.connection()
-    db = client["testpy"]
-    coll = db["test_collection"]
+@app.route('/verse/<id>', methods=['GET'])
+def select_vers(id):
+    verse = mongo.db.test_collection.find_one({'_id': ObjectId(id)})
 
-    if request.json:
-        try:
-            mongoapp.update_one(coll, request.json[0], request.json[1])
-            return 'OK'
-        except: return 'Bad Request: The data must be a JSON Array --> [query, modifications]'
+    resp = dumps(verse)
+    return resp
 
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_verse(id):
+    mongo.db.test_collection.delete_one({'_id': ObjectId(id)})
+
+    resp = jsonify('User deleted successfully')
+    resp.status_code = 200
+    return resp
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_vers(id):
+    _id = id
+    _json = request.json
+    _author = _json['autor']
+    _text = _json['texto']
+
+    if _author and _text:
+        mongo.db.test_collection.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'autor': _author, 'texto': _text}})
+
+        resp = jsonify('Verse updated successfully')
+        resp.status_code = 200
+        return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/mongoapp.py
+++ app/mongoapp.py
@@ -1,10 +1,9 @@
-import pymongo
-import datetime
+import pymongo as pm
 from bson.json_util import dumps
 
 def connection():
     try:
-        client = pymongo.MongoClient(
+        client = pm.MongoClient(
             "mongodb://andrey:mongopass@cluster0-shard-00-00-ccvwf.mongodb.net:27017,cluster0-shard-00-01-ccvwf.mongodb.net:27017,cluster0-shard-00-02-ccvwf.mongodb.net:27017/testpy?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
     except: print('Database connect error')
     return client
@@ -12,41 +11,31 @@ def connection():
 # ------------------- OPERATIONS -------------------
 # INSERT
 def create_one(coll, data):
-    try:
-        op = coll.insert_one(data)
-        return op
-    except: print('Insert operation error')
+    op = coll.insert_one(data)
+    return op
 
 def create_many(coll, data):
-    try:
-        op = coll.insert_many(data)
-        return op
-    except: print('Insert many operation error')
+    op = coll.insert_many(data)
+    for x in op:
+        print(x)
+    return op.inserted_ids
 
 # SELECT
 def select(coll, data):
-    try:
-        op = dumps(coll.find(data))
-        return op
-    except: print('Find operation error')
+    op = dumps(coll.find(data))
+    return op
 
 def select_all(coll):
-    try:
-        op = dumps(coll.find())
-        return op
-    except: print('Find all operation error')
+    op = dumps(coll.find())
+    return op
 
 # UPDATE
 def update_one(coll, query, data):
-    try:
-        coll.update_one(query, { '$set': data })
-    except: print('Update operation error')
+    coll.update_one(query, { '$set': data })
 
 # DELETE 
 def delete(coll, query):
-    try:
-        coll.delete_one(query)
-    except: print('Delete operation error')
+    coll.delete_one(query)
 
 
 """ x = register_many(coll, post)
