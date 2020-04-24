a7085f3ac3eedd560b8e21d71055097f80a16248
zAndreyG <andreygo20@gmail.com>
2020-04-24 14:24:29 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,6 +11,8 @@ app.secret_key = "secretkey"
 mongoDev = PyMongo(app, uri="mongodb://andrey:mongopass@cluster0-shard-00-00-ccvwf.mongodb.net:27017,cluster0-shard-00-01-ccvwf.mongodb.net:27017,cluster0-shard-00-02-ccvwf.mongodb.net:27017/testDev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 mongoProd = PyMongo(app, uri="mongodb://andrey:mongopass@cluster0-shard-00-00-ccvwf.mongodb.net:27017,cluster0-shard-00-01-ccvwf.mongodb.net:27017,cluster0-shard-00-02-ccvwf.mongodb.net:27017/testProd?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 
+# ======================================  DEV  ====================================== #
+
 @app.route('/dev/create', methods=['POST'])
 def insert_vers_dev():
     if isinstance(request.json, list) == False:
